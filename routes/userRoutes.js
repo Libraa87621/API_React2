4454540f744ca5c19bf4b2f0e6e007fba4b2f40a
Libraaa <annvps41218@gmail.com>
2025-03-13 09:40:00 +0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,38 @@
 const express = require('express');
-const User = require("../models/userModel");
 const userModel = require('../models/userModel');
+
+const JWT = require('jsonwebtoken');
+const config = require("../ultil/tokenConfig");
+const { token } = require('morgan');
+
+
 require('dotenv').config();
 
 const router = express.Router();
 
 
-// Đăng nhập và tạo token + refresh token
 router.post('/login', async function (req, res) {
     try {
         const { email, password } = req.body;
       
-        const user = userModel.find({email: email, password: password});
-        if (!user || password !== user.password) {
+        // Sử dụng `await` để lấy dữ liệu từ database
+        const user = await userModel.findOne({email: email, password: password});
+        
+        if (!user) {
             return res.status(400).json({ message: "Email hoặc mật khẩu không đúng!" });
         }
 
-        res.json({ message: "Đăng nhập thành công!", accessToken, refreshToken });
+        // Tạo token nếu đăng nhập thành công
+        const token = JWT.sign({ email: email }, config.SECRETKEY, { expiresIn: '30s' });
+        const refreshToken = JWT.sign({ email: email }, config.SECRETKEY, { expiresIn: '1d' });
+
+        res.json({ message: "Đăng nhập thành công!", token: token, refreshToken: refreshToken });
+
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
+
 
 // API lấy danh sách user (yêu cầu token)
 router.get('/users', authenticateToken, async (req, res) => {
